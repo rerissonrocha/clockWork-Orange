0ad06ac58d6ee58023682cf0e5f8c332b7b5a245
rerissonrocha <rerissonsousa@gmail.com>
2018-09-17 11:51:42 +0100

    Buttons organization and initial thoughts about sound stuff

--- js/clockworkOrange.js
+++ js/clockworkOrange.js
@@ -12,6 +12,45 @@ function addZero(i) {
     }
     return i;
 }
+function  beep(){
+  var snd = new Audio("./snd/alarm1.wav");
+  snd.play();
+}
+function alarmDefinition(){
+  document.getElementById("hour", "date").style.display = "none";
+  document.getElementById("backBtn").style.display = "inline";
+}
+function AlarmRedefinition(){
+  document.getElementById("hour", "date").style.display = "block";
+  document.getElementById("backBtn").style.display = "none";
+}
+function btnOrganization(i){
+  if (i == 0){
+    document.getElementById("img0").src = "img/alarmClockBlack.png";
+    document.getElementById("img1").src = "img/timerWhite.png";
+    document.getElementById("img2").src = "img/stopwatchWhite.png";
+    document.getElementById("btn0").className = "button active";
+    document.getElementById("btn1").className = "button";
+    document.getElementById("btn2").className = "button";
+  }
+  else if (i == 1){
+    document.getElementById("img0").src = "img/alarmClockWhite.png";
+    document.getElementById("img1").src = "img/timerBlack.png";
+    document.getElementById("img2").src = "img/stopwatchWhite.png";
+    document.getElementById("btn0").className = "button";
+    document.getElementById("btn1").className = "button active";
+    document.getElementById("btn2").className = "button";
+  }
+  else if (i == 2){
+    document.getElementById("img0").src = "img/alarmClockWhite.png";
+    document.getElementById("img1").src = "img/timerWhite.png";
+    document.getElementById("img2").src = "img/stopwatchBlack.png";
+    document.getElementById("btn0").className = "button";
+    document.getElementById("btn1").className = "button";
+    document.getElementById("btn2").className = "button active";
+  }
+}
+
 function weekDayTranslation(i){
   if (i == 0) {
     i = "Domingo";
@@ -75,5 +114,4 @@ function monthTranslation(i){
   }
   return i;
 }
-
 setInterval(function(){getTime()}, 1000);
